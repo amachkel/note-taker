67c9eb762c5cf09fdebaa67486d0c78bced9ef2d
amachkel <amachkel@gmail.com>
2022-03-15 17:10:37 -0400

    organized files. Must save notes to json

---
+++ api/notes.js
@@ -0,0 +1,42 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+const uuid = require('uuid');
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
+
+// GET Route for retrieving all notes
+notes.get('/', (req, res) => {
+    console.info(`${req.method} request received for notes`);
+  
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  });
+
+// POST Route for adding notes
+notes.post('/api/notes', (req, res) => {
+    // Log that a POST request was received
+    console.info(`${req.method} request received to add note`);
+  
+    // Destructuring assignment for the items in req.body
+    const { title, text } = req.body;
+  
+    // If all the required properties are present
+    if (title && text) {
+      // Variable for the object we will save
+      const newNote = {
+        title,
+        text,
+        feedback_id: uuid(),
+      };
+  
+      readAndAppend(newNote, './db/db.json');
+  
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+  
+      res.json(response);
+    } else {
+      res.json('Error in posting feedback');
+    }
+  });
+module.exports = notes;
--- server.js
+++ server.js
@@ -1,12 +1,21 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+uuidv4();
+// enivornment variable: PORT, 
+// global object: process, 
+// obj property 'env': environment variable
 const PORT = process.env.PORT || 3001;
+// to import modular router for /notes
+const notesRouter = require('./api/notes');
+
 
 // middleware
 app.use(express.json());
-app.use(express.static('public'))
-
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/notes', notesRouter);
 // specify path, then a callback function, or route handler
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "public/notes.html"));
@@ -14,8 +23,10 @@ app.get("/notes", (req, res) => {
 
 // GET route for homepage
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "public/index.html"));
+});
+
+// GET /api/notes should read the db.json file and return all saved notes as JSON.
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT}`)
